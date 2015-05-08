76c528b4685e12523589c80832a73cc56d0acd27
Teodor Talov <teodor@talov.com>
2015-05-08 02:31:05 -0400

    Refactored authentication to suport multiple clients - Direct, OAuth, etc.

--- src/Citrix/Authentication/Client.php
+++ src/Citrix/Authentication/Client.php
@@ -8,7 +8,37 @@
 
 namespace Citrix\Authentication;
 
-
+/*
+ * Wrapper class for all authentication methods.
+ * It could be used for a single point of entry foo authentication functionality.
+ */
 class Client {
+
+  private $authentication;
+  public function __construct($authentication = 'Direct'){
+
+    $this->setAuthentication($authentication);
+  }
+
+  public function auth(){
+    return $this->getAuthentication()->auth();
+  }
+  /**
+   * @return mixed
+   */
+  public function getAuthentication()
+  {
+    return $this->authentication;
+  }
+
+  /**
+   * @param string $authentication
+   */
+  public function setAuthentication($authentication)
+  {
+    $class = '\\Citrix\\Authentication\\' . $authentication;
+    $this->authentication = new $class();
+    return $this;
+  }
 
 }
